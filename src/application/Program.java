34791ecfcfa1c62b8e7c8c9a8b689358d5d33ae4
4Miguel2 <delgdevoleper@gmail.com>
2024-01-27 17:54:55 -0300

    Simple Integer test

---
+++ src/application/Program.java
@@ -0,0 +1,37 @@
+package application;
+
+import services.CalculationService;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        List<Integer> list = new ArrayList<>();
+
+        String path = "C:\\temp\\in.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            String line = br.readLine();
+            while (line != null) {
+                list.add(Integer.parseInt(line));
+                line = br.readLine();
+            }
+
+            Integer x = CalculationService.max(list);
+            System.out.print("Max: ");
+            System.out.println(x);
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
